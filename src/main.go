b62c9372386570a6fab7fd579d63cfb2ddd000d5
Connor Henderson <connor.js.henderson@gmail.com>
2019-01-28 18:44:50 -0500

    Filled out API handlers

--- src/main.go
+++ src/main.go
@@ -9,7 +9,13 @@ import (
 	"net/http"
 )
 
+var db *DB
+
 func main() {
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+	defer db.Close()
 	var router = mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
 	router.HandleFunc("/getall/{user}", returnAll).Methods("GET")
@@ -25,8 +31,83 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func returnAll(w http.ResponseWriter, r *http.Request) {
+	var uid int
+	var link string
+	var links []string
+	uidSelect, err := db.Prepare("SELECT uid FROM users where uname = ?")
+	checkErr(err)
+	linkSelect, err := db.Prepare("SELECT link FROM links WHERE uid = ?")
+	checkErr(err)
+
+	uName := mux.Vars(r)["user"]
+	rows, err := uidSelect.Query(uName)
+	checkErr(err)
+	rows.Next()
+	err = rows.Scan(&uid)
+	checkErr(err)
+
+	rows, err = linkSelect.Query(uid)
+	checkErr(err)
+	for rows.Next() {
+		rows.Scan(&link)
+		append(links, link)
+	}
+	json.NewEncoder(w).Encode(links)
 
 }
 func handleDelete(w http.ResponseWriter, r *http.Request) {
 
+	var uid int
+
+	vars := r.URL.Query()
+	uname := vars.Get("name")
+	link := vars.Get("link")
+	//would need to check auth here
+	uidSelect, err := db.Prepare("SELECT uid FROM users where uname = ?")
+	checkErr(err)
+	linkDelete, err := db.Prepare("DELETE FROM links WHERE uid = ? AND link = ?")
+	checkErr(err)
+
+	rows, err := uidSelect.Query(uName)
+	checkErr(err)
+	rows.Next()
+	err = rows.Scan(&uid)
+	checkErr(err)
+
+	res, err := linkDelete.Exec(uid, link)
+	checkErr(err)
+	msg := fmt.Sprintf("Rows deleted: %d", res.RowsAffected())
+	json.NewEncoder(w).Encode(map[string]string{"message": msg})
+
+}
+
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	var uid int
+
+	vars := r.URL.Query()
+	uname := vars.Get("name")
+	link := vars.Get("link")
+
+	uidSelect, err := db.Prepare("SELECT uid FROM users where uname = ?")
+	checkErr(err)
+	linkAdd, err := db.Prepare("INSERT INTO links (uid, link) VALUES (?, ?)")
+	checkErr(err)
+
+	rows, err := uidSelect.Query(uName)
+	checkErr(err)
+	rows.Next()
+	err = rows.Scan(&uid)
+	checkErr(err)
+
+	res, err := linkAdd.Exec(uid, link)
+	checkErr(err)
+	msg := fmt.Sprintf("Rows added: %d", res.RowsAffected())
+	json.NewEncoder(w).Encode(map[string]string{"message": msg})
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
